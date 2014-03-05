811a59da9716825cf6c4600329b2e2d4fcd23e0b
LI Yi <eagle.liyi@gmail.com>
2014-03-05 22:21:18 +0800

    parse wdj, get needed info

--- wdj_parser/parser.py
+++ wdj_parser/parser.py
@@ -2,14 +2,44 @@
 from applications.models import Application
 
 import re
+import urllib
 
-def parse_wdj_url(icon):
-    icon.name = '淘宝彩票'
-    icon.original_icon_image = 'http://img.wdjimg.com/mms/icon/v1/b/6d/187c16d748f4d2843da62d51abc746db_256_256.png'
-    icon.description = '淘宝彩票是一个简单的口袋投注站，它能帮你摇摇手机完成投注，动动手指完成守号，一个按键完成付款，贴心推送告知中奖，大奖专人通知，万元以下奖金打入支付宝，另外你还可以和你的朋友通过微博，微信一起分享中奖的喜悦，购彩的乐趣。当遇到节假日，您朋友生日，更是可以通过手机批量送彩票送祝福。淘宝彩票由淘宝官方出品，服务广大彩民的一款彩票购买、开奖查询、订单查询的一站式彩票客户端。淘宝福地，精“彩”不断！下一个大奖属于你！'
-    icon.download_count = '106 万'
-    icon.package_name = 'com.taobao.caipiao'
-    icon.status = Application.CREATE
+def parse_wdj_url(application):
+    url = application.wandoujia_url
+    application_html = urllib.urlopen(url).read()
 
-    return icon
+    application.name = _get_application_name(application_html)
+    application.original_icon_image = _get_app_original_icon(application_html)
+    application.description = _get_application_description(application_html)
+    application.download_count = _get_application_download_count(application_html)
 
+    application.package_name = 'com.taobao.caipiao'
+    application.status = Application.CREATE
+
+    return application
+
+def _get_application_name(application_html):
+    pattern_string = '<p class="app-name">[\s\S]*?<span class="title" itemprop="name">(\S*?)</span>'
+    return _reg_search(pattern_string, application_html)
+
+def _get_app_original_icon(application_html):
+    pattern_string = '<div class="app-icon">[\s\S]*?<img src="(\S*?)"'
+    return _reg_search(pattern_string, application_html)
+
+def _get_application_description(application_html):
+    pattern_string = 'itemprop="description">[\s\S]*?([\s\S]*?)</div>'
+    return _reg_search(pattern_string, application_html)
+
+def _get_application_download_count(application_html):
+    pattern_string = '<i itemprop="interactionCount" content="UserDownloads:(\d*?)"'
+    return _reg_search(pattern_string, application_html)
+
+
+def _reg_search(pattern_string, application_html):
+    pattern = re.compile(pattern_string)
+    result = pattern.search(application_html)
+
+    if result is not None:
+        return result.group(1)
+    else:
+        return 'Null'
